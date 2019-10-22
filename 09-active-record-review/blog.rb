138083d3b1b92acbef9843f5839888e7d78175d9
Lyn Muldrow <lyn.muldrow@flatironschool.com>
2019-10-22 08:48:01 -0400

    error free activerecord code

--- 09-active-record-review/blog.rb
+++ 09-active-record-review/blog.rb
@@ -1,12 +1,13 @@
 # Here are a set of deliverables that we'll work on and run together.
 # Check out the README for more info. 
 
+# Add our required files (one error was here- Javi said it! We need to require ActiveRecord before using it.)
+require 'sqlite3'
+require 'active_record'
 
-# Set up a database that resides in RAM
-ActiveRecord::Base.establish_connection (
-    adapter: 'sqlite3',
-    database: ':memory:'
-)
+
+# Set up a database that resides in RAM - (this is where one error was located- spacing!)
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:')
 
 
 # Set up database tables and columns
@@ -14,15 +15,15 @@ ActiveRecord::Schema.define do
     create_table "posts", force: :cascade do |t|
         t.text "content"
         t.string "name"
-        t.integer "post_id"
         t.datetime "created_at", null: false
         t.datetime "updated_at", null: false
-        t.index ["post_id"], name: "index_comment_on_post_id"
     end 
 
     create_table "comments", force: :cascade do |t|
         t.text "content"
-        t.string "title"
+        t.integer "post_id" # and here is how we create relationships- a comment BELONGS TO a post, aligning with it's id
+        t.index ["post_id"], name: "index_comment_on_post_id"
+        t.string "title" # look right here- we said that to have a comment we need a title and content, but didn't add titles!
         t.datetime "created_at", null: false
     end
 end
--- 09-active-record-review/our-code.rb
+++ 09-active-record-review/our-code.rb
@@ -2,6 +2,10 @@
 # Let's add some content. 
 
 
+# Deliverables from our-code.rb! 
+#Here's the code that works. Look through this and compare it to what we went over in class to understand the errors.
+
+require './blog' # we need to require `blog.rb` with the declarations that we made about posts and comments.
 
 # Create a new post.
 
@@ -10,7 +14,8 @@ post = Post.new
 
 # Give the post a title. 
 
-post.title = "YEET!"
+post.name = "YEET!" # we originally had "post.title" - did we call it NAME, or title? Always check what you wrote.
+
 
 # Add some body content to the post. 
 
@@ -20,6 +25,7 @@ post.content = "Today, we yeeted without our haw. Yesterday, the haw was yeeted.
 # Save the post to persist it in our local database (stored in RAM)
 
 post.save
+
 
 # Add a comment to our post. 
 
@@ -33,12 +39,6 @@ post.comments.create(content: "I like yeetin' haws. :D")
 
 # Show EACH post and the comments for each in output. 
 
-post.find_each do |post|
+Post.find_each do |post|
     p post.comments
 end
-
-
-# What are some other CRUD operations that we can try? 
-
-
-
